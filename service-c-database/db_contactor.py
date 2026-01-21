a99e2fcfb1218488f3851e326b4f4511ecb31404
Yosef shoval <ys0542522504@gmail.com>
2026-01-21 12:02:16 +0200

    fix the connection to database, insert works as expected

--- service-c-database/db_contactor.py
+++ service-c-database/db_contactor.py
@@ -2,63 +2,81 @@ import os
 import mysql.connector
 
 
-
-
-SQL_HOST = os.getenv("SQL_HOST")
-SQL_USER = os.getenv("SQL_USER")
-SQL_PASSWORD = os.getenv("SQL_PASSWORD")
-SQL_DATABASE = os.getenv("SQL_DATABASE")
-
-
-def connect_to_database():
-    db_connector = mysql.connector.connect(
-    host=SQL_HOST,
-    user=SQL_USER,
-    password=SQL_PASSWORD
-    )
-    if not db_connector.is_connected:
-        raise ConnectionError("Could not connect to database")
-
-    create_table = """CREATE TABLE IF NOT EXISTS records (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    timestamp DATETIME  NOT NULL,
-    location_name VARCHAR(255) NOT NULL,
-    country VARCHAR(255) NOT NULL,
-    latitude FLOAT NOT NULL,
-    longitude FLOAT NOT NULL,
-    temperature FLOAT NOT NULL,
-    wind_speed FLOAT NOT NULL,
-    humidity INT NOT NULL,
-    temperature_category VARCHAR(255) NOT NULL,
-    wind_category VARCHAR(255) NOT NULL
-    )"""
-    cursor = db_connector.cursor().execute(create_table)
-    cursor.close()
-    db_connector.commit()
-
-
-    return db_connector
+SQL_HOST = os.getenv("SQL_HOST", 'localhost')
+SQL_PORT = os.getenv("SQL_PORT", 3307)
+SQL_USER = os.getenv("SQL_USER", 'root')
+SQL_PASSWORD = os.getenv("SQL_PASSWORD", '')
+SQL_DATABASE = os.getenv("SQL_DATABASE", 'weather')
 
 
 
-def insert_records(records, db_connector):
-    cursor = db_connector.cursor()
-    sql = "INSERT INTO customers () VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = [(row for row in record) for record in records]
-    cursor.executemany(sql, val)
-    cursor.close()
-    db_connector.commit()
+class DbConnection:
+    def __init__(self, host, port, user, password, database):
+        self.config = {
+            'host': host,
+            'port': port,
+            'user': user,
+            'password': password
+        }
+        self.database = database
+        self.connection = None
 
 
-    return cursor.rowcount
+    def get_connection(self):
+        self.connection = mysql.connector.connect(**self.config)
+
+        if not self.connection.is_connected:
+            raise ConnectionError("Couldn't connect to the database")
+
+        cursor = self.connection.cursor()            
+        return cursor
 
 
 
+    def create_table(self):
+        cursor = self.get_connection()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.database}")
+
+        cursor.execute(f"USE {self.database}")
+
+        create_statement = """
+            CREATE TABLE IF NOT EXISTS records (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            timestamp DATETIME  NOT NULL,
+            location_name VARCHAR(255) NOT NULL,
+            country VARCHAR(255) NOT NULL,
+            latitude FLOAT NOT NULL,
+            longitude FLOAT NOT NULL,
+            temperature FLOAT NOT NULL,
+            wind_speed FLOAT NOT NULL,
+            humidity INT NOT NULL,
+            temperature_category VARCHAR(255) NOT NULL,
+            wind_category VARCHAR(255) NOT NULL
+            );"""
+
+        cursor.execute(create_statement)
+        self.connection.commit()
+    
 
 
+    def insert_records(self, records):
+        cursor = self.get_connection()
+        cursor.execute(f"USE {self.database}")
 
+        insert_statement = """INSERT INTO records (
+                timestamp, location_name, country,
+                latitude, longitude, temperature, wind_speed, humidity, temperature_category, wind_category)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ;"""
 
+        values = [tuple(record.values()) for record in records]
 
+        if len(values) > 1:
+            cursor.executemany(insert_statement, values)
+        elif len(values) == 1:
+            cursor.execute(insert_statement, values[0])
+        else:
+            print('There are no record to insert')
 
-
-
+        self.connection.commit()
+        return cursor.rowcount
--- service-c-database/main.py
+++ service-c-database/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from db_contactor import *
+
 
 app = FastAPI()
 
-
+DB_connector = DbConnection(
+    host=SQL_HOST,
+    port=SQL_PORT,
+    user=SQL_USER,
+    password=SQL_PASSWORD
+)
 
 @app.post("/records")
 def records(records_list: list):
-    pass
-    
+    try:
+        DB_connector.create_table()
+        DB_connector.insert_records(records_list)
+    except ConnectionError as e:
+        raise HTTPException(status_code=500, detail={' Database  Connection Error':str(e)})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
